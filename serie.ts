6408fce078b9bbdd85de3e85a53860b4810bb476
Vanesa M. <141964604+vanjk@users.noreply.github.com>
2024-11-05 19:27:51 -0500

    Re subida de archivos

---
+++ serie.ts
@@ -0,0 +1,9 @@
+export class Serie {
+    
+
+
+    constructor(public id: number, public nombre: string, public canalTV: string, public temporadas: number, 
+                public descripcion: string, public direcciónURL: string, public imgURL: string) {
+        
+    }
+}

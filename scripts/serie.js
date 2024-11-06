89fc30df8fd62561aba4f9ae03493b5f047421cd
Vanesa Montaño <k.montano@uniandes.edu.co>
2024-11-05 19:34:59 -0500

    Final Taller1

---
+++ scripts/serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, nombre, canalTV, temporadas, descripcion, direcciónURL, imgURL) {
+        this.id = id;
+        this.nombre = nombre;
+        this.canalTV = canalTV;
+        this.temporadas = temporadas;
+        this.descripcion = descripcion;
+        this.direcciónURL = direcciónURL;
+        this.imgURL = imgURL;
+    }
+    return Serie;
+}());
+export { Serie };
